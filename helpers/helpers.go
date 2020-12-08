1a59c986b9be3142501ff529e1a91eb91308855f
Drew Boyuka <drew@pendo.io>
2020-12-08 00:59:52 -0500

    Add Split2/Split3 helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -24,3 +24,23 @@ func AtoiOrZero(s string) int {
 func Between(x, low, high int) bool {
 	return x >= low && x <= high
 }
+
+func Split2(s, sep string) (a, b string) {
+	idx := strings.Index(s, sep)
+	if idx == -1 {
+		return s, ""
+	} else {
+		return s[:idx], s[idx+len(sep):]
+	}
+}
+
+func Split3(s, sep1, sep2 string) (a, b, c string) {
+	if idx1 := strings.Index(s, sep1); idx1 != -1 {
+		a = s[:idx1]
+		b, c = Split2(s[idx1+len(sep1):], sep2)
+	} else {
+		b = ""
+		a, c = Split2(s, sep2)
+	}
+	return a, b, c
+}
