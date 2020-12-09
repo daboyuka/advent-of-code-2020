2b59dd90dc59d491789c17c0a571701d03d0d45f
Drew Boyuka <drew@pendo.io>
2020-12-09 00:44:47 -0500

    Problems 9a and 9b in Go

--- 9.go
+++ 9.go
@@ -1,22 +1,83 @@
 package aoc2020
 
-type thingy struct{}
+import (
+	"fmt"
 
-func parseThingy(line string) thingy { return thingy{} }
+	"aoc2020/helpers"
+)
 
-func parseLines(lines []string) (items []thingy) {
+func parseLines(lines []string) (nums []int) {
 	for _, line := range lines {
-		items = append(items, parseThingy(line))
+		nums = append(nums, helpers.MustAtoi(line))
 	}
-	return items
+	return nums
+}
+
+func check(prefix []int, target int) bool {
+	for _, a := range prefix {
+		for _, b := range prefix {
+			if a != b && a+b == target {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func Problem9a(lines []string) {
-	items := parseLines(lines)
-	_ = items
+	nums := parseLines(lines)
+
+	for i := 25; i < len(nums); i++ {
+		prefix := nums[i-25 : i]
+		if !check(prefix, nums[i]) {
+			fmt.Println(nums[i])
+			return
+		}
+	}
+}
+
+func findRange(nums []int, target int) (first, last int) {
+	accums := make([]int, len(nums))
+	for i, x := range nums {
+		accums[i] = x
+		if i > 0 {
+			accums[i] += accums[i-1]
+		}
+	}
+
+	for i := range accums {
+		for j := range accums[i+1:] {
+			if accums[j+i+1]-accums[i] == target {
+				return i, j + i + 1
+			}
+		}
+	}
+
+	return -1, -1
 }
 
 func Problem9b(lines []string) {
-	items := parseLines(lines)
-	_ = items
+	nums := parseLines(lines)
+
+	target := -1
+	for i := 25; i < len(nums); i++ {
+		prefix := nums[i-25 : i]
+		if !check(prefix, nums[i]) {
+			target = nums[i]
+			break
+		}
+	}
+
+	first, last := findRange(nums, target)
+	min, max := -1, -1
+	for _, x := range nums[first:last] {
+		if min == -1 || min > x {
+			min = x
+		}
+		if max == -1 || max < x {
+			max = x
+		}
+	}
+
+	fmt.Println(first, last, min, max)
 }
